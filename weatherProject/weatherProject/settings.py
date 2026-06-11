c645c5f9f1c540e7c1bb83bff8420aef1ba562b0
Nysa <dikshitanextdoor@gmail.com>
2026-06-11 21:02:20 +0530

    enable debug

--- weatherProject/weatherProject/settings.py
+++ weatherProject/weatherProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0bqzmfy$@hhso_(bt#z*(os-o*%3)g0uuxg*-jt8^x%-#eopuj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
