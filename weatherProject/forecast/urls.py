19fef17360239050147dff26ba5a87ed238c59b5
Nysa <dikshitanextdoor@gmail.com>
2026-06-11 14:04:12 +0530

    fix allowed hosts

--- weatherProject/forecast/urls.py
+++ weatherProject/forecast/urls.py
@@ -2,8 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.weather_view, name= 'Weather View'),
-    path('get-weather/', views.get_weather_api),
-    
+    path('', views.weather_view, name='weather'),
 
+    path(
+        'weather/location/',
+        views.weather_by_location,
+        name='weather_by_location'
+    ),
+
+    path(
+        'api/weather/',
+        views.get_weather_api,
+        name='weather_api'
+    ),
 ]
